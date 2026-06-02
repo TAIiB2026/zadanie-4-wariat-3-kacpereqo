4d1914eb9e96a822a5c2b49bb6b9069c5c2fe573
Kacperek <61110867+kacpereqo@users.noreply.github.com>
2026-06-02 14:20:02 +0200

    feat: zdane hehe

--- Client/src/app/classes/film.class.ts
+++
@@ -1,3 +0,0 @@
-export class FilmClass {
-    constructor(public id: number, public tytul: string, public cena: number, public dataPremiery: Date) {}
-}
--- Client/src/app/interfaces/get-data.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from "rxjs";
-import { FilmClass } from "../classes/film.class";
-
-export interface GetDataInterface {
-    Get(): Observable<FilmClass[]>;
-    GetByID(id: number): Observable<FilmClass>;
-}
